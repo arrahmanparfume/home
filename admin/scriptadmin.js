fd68e4083dbb197c0c05a5d31a605411dcd0210e
arrahmanparfume <bimogit@gmail.com>
2024-10-11 14:08:16 +0700

    scriptadmin.js

--- admin/scriptadmin.js
+++ admin/scriptadmin.js
@@ -1,39 +1,8 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxG-dUEAbgUCWdV_zm-1YSeUHriisq7W7eMnyG_ii7gOMBHOv44xRbpHCQyQ3WXrP5VaQ/exec'; // Masukkan Google Apps Script URL
 const form = document.getElementById('inputForm');
-const scriptURL = 'https://script.google.com/macros/s/AKfycbycFpqWJQBZZ0JFNvqGsQSdm0lWYiBCcsLGCDenssM7icSVILt2AVvb2le_TvydMZi4zQ/exec';
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    const formData = new FormData(form);
-
-    const fileInput = document.querySelector('input[type="file"]');
-    const file = fileInput.files[0];
-    const reader = new FileReader();
-
-    reader.onloadend = function () {
-        const base64String = reader.result.replace('data:', '').replace(/^.+,/, '');
-
-        formData.append('gambar', base64String);
-        formData.append('gambarMimeType', file.type);
-        formData.append('gambarName', file.name);
-
-        // Kirim data
-        fetch(scriptURL, {
-            method: 'POST',
-            body: formData,
-        })
-            .then(response => alert('Data berhasil dikirim!'))
-            .catch(error => console.error('Error!', error.message));
-    };
-
-    if (file) {
-        reader.readAsDataURL(file); // Konversi file gambar menjadi Base64
-    } else {
-        // Jika tidak ada gambar, kirim form langsung
-        fetch(scriptURL, {
-            method: 'POST',
-            body: formData,
-        })
-            .then(response => alert('Data berhasil dikirim!'))
-            .catch(error => console.error('Error!', error.message));
-    }
-});
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => alert('Data berhasil dikirim!'))
+                .catch(error => console.error('Error!', error.message));
+        });
