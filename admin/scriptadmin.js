7b074d7ed8e19e28183061f22061c983f2117360
arrahmanparfume <bimogit@gmail.com>
2024-10-12 10:25:26 +0700

    scriptadmin.js

--- admin/scriptadmin.js
+++ admin/scriptadmin.js
@@ -1,8 +1,14 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxG-dUEAbgUCWdV_zm-1YSeUHriisq7W7eMnyG_ii7gOMBHOv44xRbpHCQyQ3WXrP5VaQ/exec'; // Masukkan Google Apps Script URL
 const form = document.getElementById('inputForm');
-        form.addEventListener('submit', e => {
-            e.preventDefault();
-            fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-                .then(response => alert('Data berhasil dikirim!'))
-                .catch(error => console.error('Error!', error.message));
-        });
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            alert('Data berhasil dikirim!');
+
+            // Mengosongkan input setelah berhasil mengirim
+            form.reset(); // Mengosongkan semua input di dalam form
+        })
+        .catch(error => console.error('Error!', error.message));
+});
